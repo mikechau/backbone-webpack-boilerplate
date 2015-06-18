f3d0de89082d7f4924f88e8e45c842f9e46e95d4
Mike Chau <mike@chau.io>
2015-06-18 14:39:49 -0500

    Update scripts and switch from lodash to underscore

--- dev.webpack-server.config.js
+++ dev.webpack-server.config.js
@@ -95,7 +95,6 @@ module.exports = {
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.DefinePlugin({
       "process.env": {
         NODE_ENV: JSON.stringify('development')
